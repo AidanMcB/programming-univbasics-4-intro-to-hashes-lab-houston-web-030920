8836071facec88338f6f8d4ff8d33ac405adde03
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-02 02:04:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash[key] == true
     return hash[key] += 1 
-  else 
+  elsif hash[key] == false
     return hash[key] = 1
   end 
  end
