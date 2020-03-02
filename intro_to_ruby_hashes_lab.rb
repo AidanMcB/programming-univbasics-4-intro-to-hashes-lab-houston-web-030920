4faf6bbc94f8f56599286fd76db51c639c8be5b3
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-02 01:53:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
  
- if hash[key] == true
+ if hash[key]
   return hash[key]
  else
    hash[key] = 1 
